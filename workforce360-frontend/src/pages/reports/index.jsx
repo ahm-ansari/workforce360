f0e22be0b5b48e5ab87e89678bb721e5323a4173
Muhammad Ansari <ahm.ansari.m@gmail.com>
2025-11-16 23:09:54 +0530

    Update index.jsx

--- workforce360-frontend/src/pages/reports/index.jsx
+++ workforce360-frontend/src/pages/reports/index.jsx
@@ -1,14 +1,30 @@
 import { Typography, Box} from '@mui/material';
 import AdminLayout from '@/components/layout/AdminLayout';
+import ReportKPIs from '@/components/reports/ReportKPIs';
+import ReportCharts from '@/components/reports/ReportCharts';
+import ReportHeatmap from '@/components/reports/ReportHeatmap';
+import RecentActivityTable from '@/components/reports/RecentActivityTable';
+import { Grid } from '@mui/material';
 
 export default function Page() {
 
   return (
     <AdminLayout>
       <Box className="flex">
-        <Typography variant="h4" gutterBottom>
-          Page Under Development
-        </Typography>
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12}}>
+            <ReportKPIs />
+          </Grid>
+          <Grid size={{ xs: 12, sm: 8, md: 8}}>
+            <ReportCharts />
+          </Grid>
+          <Grid size={{ xs: 12, sm: 4, md: 4}}>
+            <ReportHeatmap />
+          </Grid>
+          <Grid size={{ xs: 12}}>
+            <RecentActivityTable />
+          </Grid>
+        </Grid>
       </Box>
     </AdminLayout>
   );
