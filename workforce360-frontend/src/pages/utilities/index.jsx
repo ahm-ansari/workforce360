270e1683c193697d1eda50a83f9b32d6fc8759da
Muhammad Ansari <ahm.ansari.m@gmail.com>
2025-11-16 23:09:49 +0530

    Update index.jsx

--- workforce360-frontend/src/pages/utilities/index.jsx
+++ workforce360-frontend/src/pages/utilities/index.jsx
@@ -1,14 +1,31 @@
-import { Typography, Box} from '@mui/material';
+import {Box} from '@mui/material';
 import AdminLayout from '@/components/layout/AdminLayout';
+import UtilitiesKPIs from '@/components/utilities/UtilitiesKPIs';
+import UtilitiesTools from '@/components/utilities/UtilitiesTools';
+import UtilitiesStatusChart from '@/components/utilities/UtilitiesStatusChart';
+import UtilitiesLogs from '@/components/utilities/UtilitiesLogs';
+import {Grid} from '@mui/material';
+
 
 export default function Page() {
 
   return (
     <AdminLayout>
       <Box className="flex">
-        <Typography variant="h4" gutterBottom>
-          Page Under Development
-        </Typography>
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12}} >
+            <UtilitiesKPIs />
+          </Grid>
+          <Grid size={{ xs: 12}}>
+            <UtilitiesTools />
+          </Grid>
+          <Grid size={{ xs: 6}}>
+            <UtilitiesStatusChart />
+          </Grid>
+          <Grid size={{ xs: 12, md: 6}}>
+            <UtilitiesLogs />
+          </Grid>
+        </Grid>
       </Box>
     </AdminLayout>
   );
