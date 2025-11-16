2e3ef177ec8926c9d3b51bbeae0cc3005fdd1fc7
Muhammad Ansari <ahm.ansari.m@gmail.com>
2025-11-16 23:10:15 +0530

    Update index.jsx

--- workforce360-frontend/src/pages/invoices/index.jsx
+++ workforce360-frontend/src/pages/invoices/index.jsx
@@ -1,14 +1,23 @@
 import { Typography, Box} from '@mui/material';
 import AdminLayout from '@/components/layout/AdminLayout';
+import BillingKPIs from '@/components/billing/BillingKPIs';
+import InvoiceTable from '@/components/billing/InvoiceTable';
+import { Grid } from '@mui/material';
+
 
 export default function Page() {
 
   return (
     <AdminLayout>
       <Box className="flex">
-        <Typography variant="h4" gutterBottom>
-          Page Under Development
-        </Typography>
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12}}>
+            <BillingKPIs />
+          </Grid>
+          <Grid size={{ xs:12}}>
+            <InvoiceTable />
+          </Grid>
+        </Grid>
       </Box>
     </AdminLayout>
   );
