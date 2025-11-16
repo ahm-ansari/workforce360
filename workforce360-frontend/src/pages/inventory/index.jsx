f67b5209af9a05d158366d54b9e886eed5a43e3c
Muhammad Ansari <ahm.ansari.m@gmail.com>
2025-11-16 23:10:18 +0530

    Update index.jsx

--- workforce360-frontend/src/pages/inventory/index.jsx
+++ workforce360-frontend/src/pages/inventory/index.jsx
@@ -1,14 +1,22 @@
 import { Typography, Box} from '@mui/material';
 import AdminLayout from '@/components/layout/AdminLayout';
+import InventoryKPIs from '@/components/inventory/InventoryKPIs';
+import InventoryTable from '@/components/inventory/InventoryTable';
+import { Grid } from '@mui/material';
 
 export default function Page() {
 
   return (
     <AdminLayout>
       <Box className="flex">
-        <Typography variant="h4" gutterBottom>
-          Page Under Development
-        </Typography>
+        <Grid container spacing={2}>
+          <Grid size={{xs: 12}}>
+            <InventoryKPIs />
+          </Grid>
+          <Grid size={{xs: 12}}>
+            <InventoryTable />
+          </Grid>
+        </Grid>
       </Box>
     </AdminLayout>
   );
