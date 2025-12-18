39f3c0799d34cdd7a0631f91744c58110c863718
Muhammad Ansari <ahm.ansari.m@gmail.com>
2025-12-18 18:29:18 +0530

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('api/visitors/', include('apps.visitors.urls')),
     path('api/projects/', include('apps.projects.urls')),
     path('api/outsourcing/', include('apps.outsourcing.urls')),
+    path('api/clients/', include('apps.clients.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
