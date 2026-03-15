e3294e31151008d0849ddfafdb4921fc36531600
Muhammad Ansari <ahm.ansari.m@gmail.com>
2026-03-15 18:49:20 +0530

    Update serializers.py

--- backend/apps/cafm/serializers.py
+++ backend/apps/cafm/serializers.py
@@ -2,8 +2,19 @@ from rest_framework import serializers
 from .models import (
     Facility, Space, Asset, MaintenanceRequest, MaintenanceLog, 
     BMSDevice, BMSTelemetry, PPMSchedule, TechnicianTraining, SystemAudit,
-    CAFMAuditLog, MaintenanceCommunication
+    CAFMAuditLog, MaintenanceCommunication, Vendor, SLAPolicy, 
+    WorkOrderStep, InventoryItem
 )
+
+class VendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vendor
+        fields = '__all__'
+
+class SLAPolicySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SLAPolicy
+        fields = '__all__'
 
 class FacilitySerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,6 +28,13 @@ class SpaceSerializer(serializers.ModelSerializer):
         model = Space
         fields = '__all__'
 
+class WorkOrderStepSerializer(serializers.ModelSerializer):
+    completed_by_name = serializers.CharField(source='completed_by.get_full_name', read_only=True)
+
+    class Meta:
+        model = WorkOrderStep
+        fields = '__all__'
+
 class MaintenanceLogSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(source='user.get_full_name', read_only=True, default='')
 
@@ -24,22 +42,43 @@ class MaintenanceLogSerializer(serializers.ModelSerializer):
         model = MaintenanceLog
         fields = '__all__'
 
+class MaintenanceCommunicationSerializer(serializers.ModelSerializer):
+    sender_name = serializers.CharField(source='sender.get_full_name', read_only=True)
+    
+    class Meta:
+        model = MaintenanceCommunication
+        fields = '__all__'
+
 class MaintenanceRequestSerializer(serializers.ModelSerializer):
     facility_name = serializers.CharField(source='facility.name', read_only=True)
     space_name = serializers.CharField(source='space.name', read_only=True)
     asset_name = serializers.CharField(source='asset.name', read_only=True)
+    vendor_name = serializers.CharField(source='vendor.name', read_only=True)
+    sla_policy_name = serializers.CharField(source='sla_policy.name', read_only=True)
     reported_by_name = serializers.CharField(source='reported_by.get_full_name', read_only=True, default='')
     assigned_to_name = serializers.CharField(source='assigned_to.user.get_full_name', read_only=True, default='')
     logs = MaintenanceLogSerializer(many=True, read_only=True)
+    steps = WorkOrderStepSerializer(many=True, read_only=True)
+    communications = MaintenanceCommunicationSerializer(many=True, read_only=True)
 
     class Meta:
         model = MaintenanceRequest
         fields = '__all__'
 
+class InventoryItemSerializer(serializers.ModelSerializer):
+    asset_name = serializers.CharField(source='asset.name', read_only=True)
+
+    class Meta:
+        model = InventoryItem
+        fields = '__all__'
+
 class AssetSerializer(serializers.ModelSerializer):
     facility_name = serializers.CharField(source='facility.name', read_only=True)
     space_name = serializers.CharField(source='space.name', read_only=True)
+    vendor_name = serializers.CharField(source='vendor.name', read_only=True)
     maintenance_history = MaintenanceRequestSerializer(source='maintenance_requests', many=True, read_only=True)
+    spare_parts = InventoryItemSerializer(many=True, read_only=True)
+    age = serializers.ReadOnlyField()
 
     class Meta:
         model = Asset
@@ -67,6 +106,9 @@ class BMSDeviceSerializer(serializers.ModelSerializer):
 
 class PPMScheduleSerializer(serializers.ModelSerializer):
     asset_name = serializers.CharField(source='asset.name', read_only=True)
+    vendor_name = serializers.CharField(source='vendor.name', read_only=True)
+    sla_policy_name = serializers.CharField(source='sla_policy.name', read_only=True)
+    assigned_to_name = serializers.CharField(source='assigned_to.user.get_full_name', read_only=True)
     
     class Meta:
         model = PPMSchedule
@@ -92,11 +134,4 @@ class CAFMAuditLogSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = CAFMAuditLog
-        fields = '__all__'
-
-class MaintenanceCommunicationSerializer(serializers.ModelSerializer):
-    sender_name = serializers.CharField(source='sender.get_full_name', read_only=True)
-    
-    class Meta:
-        model = MaintenanceCommunication
         fields = '__all__'
